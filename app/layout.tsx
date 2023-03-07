864cdb172a9744ad025d9d8338beefdfe0707a9d
Eduardo Duarte <eduardoievolucao@gmail.com>
2023-03-06 22:30:41 -0300

    edit page

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,14 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                {/* Sidebar */}
+
+                <main>
+                    {/* Header */}
+                    {children}
+                </main>
+            </body>
         </html>
     )
 }
