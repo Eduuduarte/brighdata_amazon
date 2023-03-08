3d48444335c513cb7027f8704eeaff93c921d106
Eduardo Duarte <eduardoievolucao@gmail.com>
2023-03-08 20:33:22 -0300

    created Header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import '../styles/globals.css';
 
@@ -14,11 +15,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className='flex bg-[#F7FBFF] h-screen'>
-                {/* Sidebar */}
                 <Sidebar />
 
                 <main className='p-10 max-w-7xl w-full mx-auto overflow-y-auto'>
-                    {/* Header */}
+                    <Header />
                     {children}
                 </main>
             </body>
